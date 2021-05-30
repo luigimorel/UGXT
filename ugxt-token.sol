ccb53bcf14f63bbfb0a4577026d88378334e9cee
Luigi Morel <morelmiles@gmail.com>
2021-05-30 12:43:53 +0300

    Add licence and SafeMath library

--- ugxt-token.sol
+++ ugxt-token.sol
@@ -1,9 +1,120 @@
-pragma solidity >=0.5.0 <0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
 
-/// @title A title that should describe the contract/interface
-/// @author The name of the author
-/// @notice Explain to an end user what this does
-/// @dev Explain to a developer any extra details
+
+/// @title  UGXT  
+/// @author Luigi Morel 
+/// @notice A token that tethers the value of the Ugandan Shilling (UGX) to a contract on top of the Ethereum network.
+/// @dev WIP
+
+
+
+
+//  ------------------------ SafeMath Library -------------------------
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     * Counterpart to Solidity's `-` operator.
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     * Counterpart to Solidity's `*` operator.
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+}
+
+
+interface ERC20Essential 
+{
+
+    function transfer(address _to, uint256 _amount) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 _amount) external returns (bool);
+
+}
+ 
+
+//------------------------ Contract to manage the conversion from UGX to UGXT -------------------------
 
 contract UGXT  {
  struct Transfer {
@@ -11,5 +122,27 @@ uint amountToConvert;
 uint amountToTransfer;
  }
 
- address payable public beneficiary
+ address payable public receiver;
+ uint public transferStart; 
+ uint  conversionStart;
+ uint public transferEnd;
+bool public ended; 
+
+mapping(address => Transfer[]) public transfers ;
+
+// Event for the start of the conversion 
+event TransferStarted (address sender, uint amount);
+
+event TransferEnded (address receiver, uint amount );
+
+
+    constructor (
+        uint _transactionStartTime,
+        uint _transactionEndTime,
+        address payable _receiver
+    ) public {
+         receiver = _receiver;
+         transferEnd = now + _transactionStartTime;
+         revealEnd = transferEnd + _transactionEndTime;
+    }
 }
