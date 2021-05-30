c7e5e29932da1805eda3e34dc053937f36b1f924
Luigi Morel <morelmiles@gmail.com>
2021-05-30 11:51:56 +0300

    Initial commit

---
+++ ugxt-token.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.8.0;
+
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+contract UGXT  {
+ struct Transfer {
+uint amountToConvert;
+uint amountToTransfer;
+ }
+
+ address payable public beneficiary
+}
